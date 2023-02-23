b55255f7cc122b427f74ce48d22e1fe5ce60d1bb
Iakov <project3@panargin.ru>
2023-02-23 17:27:33 +0300

    lesson08

---
+++ lesson08/managePhonebook.py
@@ -0,0 +1,69 @@
+from phonebook import phonebook
+pb = phonebook("phonebook.txt")
+
+
+def listRecordsAll():
+    print(*pb.recordList, sep='\n')
+    return True
+
+
+def inputRecord(id=None):
+    return {
+        'id': id,
+        'firstName': input("input first name:"),
+        'middleName': input("input middle name:"),
+        'lastName': input("input last name:"),
+        'phone': input("input phone:")
+    }
+
+
+def addRecord():
+    r = inputRecord()
+    pb.add(r['firstName'], r['middleName'], r['lastName'], r['phone'])
+    return True
+
+
+def removeRecord():
+    recordId = int(input("input record id:"))
+    pb.remove(recordId)
+    return True
+
+
+def editRecord():
+    rId = int(input("input record id:"))
+    print(pb.get(rId))
+    r = inputRecord(rId)
+    pb.editRecord(r)
+    return True
+
+
+def savePhonebook():
+    pb.saveToFile()
+    return True
+
+
+def exitFunc():
+    return False
+
+
+menuDict = {
+    1: {"description": "List all recorde", "func": listRecordsAll},
+    2: {"description": "Add new record", "func": addRecord},
+    3: {"description": "Remove record", "func": removeRecord},
+    4: {"description": "Edit record", "func": editRecord},
+    5: {"description": "Save phonebook", "func": savePhonebook},
+    6: {"description": "Exit", "func": exitFunc},
+}
+
+play = True
+while play:
+    for key, item in menuDict.items():
+        print(f'{key}:{item["description"]}')
+
+    try:
+        index = int(input("Please select menu item:"))
+        play = [item["func"]
+                for key, item in menuDict.items() if key == index][0]()
+    except Exception as e:
+        print(e)
+        break
---
+++ lesson08/phonebook.py
@@ -0,0 +1,46 @@
+import json
+from os import path
+
+
+class phonebook:
+    filename = None
+    recordList = []
+    autoIncrement = 0
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.recordList = []
+        if path.exists(filename):
+            self.readFromFile()
+
+    def readFromFile(self):
+        with open(self.filename) as json_file:
+            self.__dict__ = json.load(json_file)
+
+    def saveToFile(self):
+        with open(self.filename, "w", encoding="utf-8") as json_file:
+            json.dump(self, json_file,
+                      default=lambda obj: obj.__dict__, sort_keys=True, indent=4)
+
+    def add(self, firstName: str, middleName: str, lastName: str, phone: str):
+        self.autoIncrement += 1
+        record = {
+            'id': self.autoIncrement,
+            'firstName': firstName,
+            'middleName': middleName,
+            'lastName': lastName,
+            'phone': phone
+        }
+        self.recordList.append(record)
+        return True
+
+    def remove(self, recordId: int):
+        self.recordList.remove(self.get(recordId))
+
+    def get(self, recordId: int):
+        return [item for item in self.recordList if item["id"] == recordId][0]
+
+    def editRecord(self, record):
+        self.recordList.remove(
+            [item for item in self.recordList if item['id'] == record['id']][0])
+        self.recordList.append(record)
